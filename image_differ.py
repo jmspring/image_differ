3be20d10714a572f2ef2706bb5e93b9663681000
jim spring <jmspring@gmail.com>
2016-12-06 08:52:58 -0800

    revamp stats, add some console output

--- image_differ.py
+++ image_differ.py
@@ -29,13 +29,74 @@ from PIL import Image
 app = Flask(__name__)
 
 # stats
-total_images_processed = 0
-largest_image_difference = -1
-last_image_difference = -1
+stats = {
+    'processing': {
+        'total': 0,
+        'largest_difference': 0,
+        'last_difference': 0,
+        'last_timestamp': 0,
+    },
+    'queues': {
+    },
+    'eventhubs': {
+    },
+    'image_store': {
+        'read': {
+            'total': 0,
+            'last_image': None,
+            'errors': 0,
+            'last_error': None
+        },
+        'delete': {
+            'total': 0,
+            'last_image': None,
+            'errors': 0,
+            'last_error': None
+        }
+
+    }
+}
 
 # handlers
 shutdown_requested = False
 diff_thread = None
+retrieve_thread = None
+
+def stats_update_image_stats(difference, capture_time):
+    global stats
+    stats['processing']['total'] = stats['processing']['total'] + 1 
+    stats['processing']['last_difference'] = difference
+    stats['processing']['last_timestamp'] = capture_time
+    if difference > stats['processing']['largest_difference']:
+        stats['processing']['largest_difference'] = difference
+
+def stats_update_image_store(operation, name, error):
+    global stats
+    stats['image_store'][operation]['total'] = stats['image_store'][operation]['total'] + 1
+    stats['image_store'][operation]['last_image'] = name
+    if error is not None:
+        stats['image_store'][operation]['errors'] = stats['image_store'][operation]['errors'] + 1
+        stats['image_store'][operation]['last_error'] = error
+
+def stats_update_queues_and_hubs(sbtype, name, success, error):
+    global stats
+    if name not in stats[sbtype]:
+        stats[sbtype][name] = {
+            'messages': 0,
+            'errors': 0,
+            'last_error': None
+        }
+    if success:
+        stats[sbtype][name]['messages'] = stats[sbtype][name]['messages'] + 1
+    else:
+        stats[sbtype][name]['errors'] = stats[sbtype][name]['errors'] + 1
+        stats[sbtype][name]['last_error'] = error 
+
+def stats_update_queues(name, success, error = None):
+    stats_update_queues_and_hubs('queues', name, success, error)
+
+def stats_update_eventhubs(name, success, error = None):
+    stats_update_queues_and_hubs('eventhubs', name, success, error)
 
 # Retrieve configuration from environment
 def environment_variables():
@@ -119,41 +180,115 @@ def sbus_send_message(namespace, access_name, access_key, entity, body, is_event
                                      shared_access_key_name=access_name,
                                      shared_access_key_value=access_key)
     message = Message(body)
-    if not is_event_hub:
-        sbus_service.send_queue_message(entity, message)
+    error = None
+    try:
+        if not is_event_hub:
+            sbus_service.send_queue_message(entity, message)
+        else:
+            sbus_service.send_event(entity, message)
+    except Exception as e:
+        error = str(e)
+    if is_event_hub:
+        stats_update_eventhubs(entity, (error == None), error)
     else:
-        sbus_service.send_event(entity, message)
+        stats_update_queues(entity, (error == None), error)
 
 def sbus_recv_message(namespace, access_name, access_key, entity, timeout=10):
     sbus_service = ServiceBusService(namespace,
                                      shared_access_key_name=access_name,
                                      shared_access_key_value=access_key)
-    message = sbus_service.receive_queue_message(entity, False, timeout)
-    if message.body == None:
-        return None
-    return message.body
+    error = None
+    message = None
+    try:
+        message = sbus_service.receive_queue_message(entity, False, timeout)
+        if message.body == None:
+            return None
+        message = message.body
+    except Exception as e:
+        error = str(e)
+    stats_update_queues(entity, (error == None), error)
+    return message
 
 def blob_retrieve_blob_bytes(account, key, container, name):
     blob_service = BlockBlobService(account_name=account, account_key=key)
+    error = None
     try:
         blob = blob_service.get_blob_to_bytes(container, name)
-    except AzureMissingResourceHttpError:
+    except AzureMissingResourceHttpError as e:
         blob = None
-    return blob               
+        error = str(e)
+    except Exception as ex:        
+        blob = None
+        error = str(e)
+    stats_update_image_store('read', name, error)
+    return blob
 
 def blob_delete_blob(account, key, container, name):
     blob_service = BlockBlobService(account_name=account, account_key=key)
+    error = None
     try:
         blob_service.delete_blob(container, name)
-    except AzureMissingResourceHttpError:
-        # TODO - log something
-        return
+    except AzureMissingResourceHttpError as e:
+        error = str(e)
+    except Exception as ex:
+        error = str(e)
+    stats_update_image_store('delete', name, error)
 
-def image_difference_loop():
+class ProcessQueueMessages:
+    messages = None;
+    messageLock = None;
+
+    def __init__(self):
+        self.messages = {}
+        self.messageLock = threading.Lock()
+
+    def insert(self, key, message):
+        print 'key: {}, message: {}'.format(key, json.dumps(message))
+        with self.messageLock:
+            self.messages[key] = message
+
+    def retrieve(self):
+        message = None
+        with self.messageLock:
+            if len(self.messages.keys()) > 0:
+                key = sorted(self.messages.keys())[0]
+                message = self.messages.pop(key)
+                print 'key: {}, message: {}'.format(key, json.dumps(message))
+        return message
+
+def process_message_loop(messageQueue):
     global shutdown_requested
-    global last_image_difference
-    global total_images_processed
-    global largest_image_difference
+
+    env = {}
+    environment_ready = False
+    while not shutdown_requested:
+        processed_packet = False
+        if not environment_ready:
+            env = environment_variables()
+            if required_environment_vars_set(env):
+                environment_ready = True
+                continue
+            else:
+                time.sleep(0.5)
+        else:
+            message = sbus_recv_message(env['serviceBusNamespace'],
+                                            env['serviceBusSharedAccessName'],
+                                            env['serviceBusSharedAccessKey'],
+                                            env['serviceBusImageQueue'],
+                                            5)
+            packet = None
+            if message:
+                try:
+                    packet = json.loads(message)
+                except Exception as e:
+                    packet = None
+                if packet:
+                    messageQueue.insert(packet['timestamp'], packet)
+            if not packet:
+                time.sleep(0.25)
+
+def image_difference_loop(messageQueue):
+    global shutdown_requested
 
     blob_service = None
     sbus_service = None
@@ -163,77 +298,87 @@ def image_difference_loop():
     environment_ready = False
     while not shutdown_requested:
         processed_packet = False
-        
+
         if not environment_ready:
             env = environment_variables()
             if required_environment_vars_set(env):
                 environment_ready = True
                 continue
         else:
-            message = sbus_recv_message(env['serviceBusNamespace'],
-                                        env['serviceBusSharedAccessName'],
-                                        env['serviceBusSharedAccessKey'],
-                                        env['serviceBusImageQueue'],
-                                        5)
-            
+            message = messageQueue.retrieve()
             if message != None:
-                packet = json.loads(message)
+                packet = message
                 current_blob_name = packet['current_image']
                 prior_blob_name = packet['prior_image']
                 capture_time = packet['timestamp']
                 current_blob = None
                 prior_blob = None
 
+                print 'Current: {}, Prior: {}, Timestamp: {}'.format(current_blob_name, prior_blob_name, capture_time)
+            
                 # Retrieve the blobs, skip the packet if either one missing
+                start = time.time()
                 current_blob = blob_retrieve_blob_bytes(env['storageAccount'],
                                                         env['storageAccountKey'],
                                                         env['storageAccountContainer'],
                                                         current_blob_name)
+                end = time.time();
+                print 'Load current: {}'.format(end - start)
+                start = end
                 prior_blob = blob_retrieve_blob_bytes(env['storageAccount'],
                                                       env['storageAccountKey'],
                                                       env['storageAccountContainer'],
                                                       prior_blob_name)
+                end = time.time();
+                print 'Load prior: {}'.format(end - start)
+
+                if current_blob and prior_blob:
+                    # determine the difference
+                    start = time.time()
+                    difference = diff_images(current_blob.content, prior_blob.content)
+                    end = time.time()
+                    print 'Difference: {}'.format(end-start)
+                    stats_update_image_stats(difference, capture_time)
+
+                    packet = {
+                        'timestamp': capture_time,
+                        'difference': difference
+                    }
+                    if difference > env['alertThreshold']:
+                        # send a message to the notifier to alert about the difference detected
+                        sbus_send_message(env['serviceBusNamespace'],
+                                        env['serviceBusSharedAccessName'],
+                                        env['serviceBusSharedAccessKey'],
+                                        env['serviceBusNotificationQueue'],
+                                        json.dumps(packet))
+
+                    # send difference to eventhub for analysis and processing
+                    start = time.time()
+                    sbus_send_message(env['eventHubNamespace'],
+                                    env['eventHubSharedAccessName'],
+                                    env['eventHubSharedAccessKey'],
+                                    env['eventHubName'],
+                                    json.dumps(packet),
+                                    True)
+                    end = time.time()
+                    print 'Send event: {}'.format(end-start)
+
+                    processed_packet = True
+
+                # clean up
                 if prior_blob:
                     # do some clean up
+                    start = time.time()
                     blob_delete_blob(env['storageAccount'],
                                      env['storageAccountKey'],
                                      env['storageAccountContainer'],
-                                     current_blob_name)
-                if not current_blob or not prior_blob:
-                    continue;
-
-                # determine the difference
-                last_image_difference = diff_images(current_blob.content, prior_blob.content)
-                if last_image_difference > largest_image_difference:
-                    largest_image_difference = last_image_difference
-                total_images_processed = total_images_processed + 1
-
-                packet = {
-                    'timestamp': capture_time,
-                    'difference': last_image_difference
-                }
-                if last_image_difference > env['alertThreshold']:
-                    # send a message to the notifier to alert about the difference detected
-                    sbus_send_message(env['serviceBusNamespace'],
-                                      env['serviceBusSharedAccessName'],
-                                      env['serviceBusSharedAccessKey'],
-                                      env['serviceBusNotificationQueue'],
-                                      json.dumps(packet))
-
-                # send difference to eventhub for analysis and processing
-                sbus_send_message(env['eventHubNamespace'],
-                                  env['eventHubSharedAccessName'],
-                                  env['eventHubSharedAccessKey'],
-                                  env['eventHubName'],
-                                  json.dumps(packet),
-                                  True)
-
-                processed_packet = True
-                # clean up
+                                     prior_blob_name)
+                    end = time.time()
+                    print'Delete prior: {}'.format(end-start)
                 current_blob = None
                 prior_blob = None
         if not processed_packet:
-            time.sleep(0.5)
+            time.sleep(1)
 
 def create_app():
     @app.route('/config')
@@ -248,11 +393,8 @@ def create_app():
 
     @app.route('/stats')
     def stats():
-        output = 'total images: {}\n' \
-                'last image difference: {}\n' \
-                'largest different: {}\n'.format(total_images_processed,
-                                                last_image_difference,
-                                                largest_image_difference)
+        global stats
+        output = '{}\n'.format(json.dumps(stats, sort_keys=True, indent=4, separators=(',', ': ')))
         return Response(output, mimetype='text/plain')
 
     @app.route('/')
@@ -262,6 +404,7 @@ def create_app():
 
     def interrupt():
         global diff_thread
+        global retrieve_thread
         global shutdown_requested
 
         shutdown_requested = True
@@ -272,8 +415,12 @@ def create_app():
 
     def start_differ():
         global diff_thread
-        diff_thread = threading.Thread(target=image_difference_loop)
+        global retrieve_thread
+        messageQueue = ProcessQueueMessages()
+        diff_thread = threading.Thread(target=image_difference_loop, args=(messageQueue,))
         diff_thread.start()
+        retrieve_thread = threading.Thread(target=process_message_loop, args=(messageQueue,))
+        retrieve_thread.start()
 
     start_differ()
     atexit.register(interrupt)
